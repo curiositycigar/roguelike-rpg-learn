91d86b05d1be8fbf6fed20ae653c2f1895f2576d
youwugang <youwugang@onething.net>
2021-11-26 01:38:30 +0800

    feat: 角色解锁功能

---
+++ Assets/Scripts/CharacterUnlockCage.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUnlockCage : MonoBehaviour
+{
+    private bool canUnlock;
+    public GameObject message;
+
+    public PlayerSelector[] characterSelects;
+    private PlayerSelector playerToUnlock;
+
+    public SpriteRenderer cagedSR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerToUnlock = characterSelects[Random.Range(0, characterSelects.Length)];
+        cagedSR.sprite = playerToUnlock.playerToSpawn.bodySR.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canUnlock)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Instantiate(playerToUnlock, transform.position, transform.rotation);
+
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canUnlock = true;
+            message.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canUnlock = false;
+            message.SetActive(false);
+        }
+    }
+}
