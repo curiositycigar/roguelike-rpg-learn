35d5e85433cede2774ea911b6f1f834d882972dd
youwugang <youwugang@onething.net>
2021-12-01 01:36:47 +0800

    fix: Boss行动

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -8,6 +8,19 @@ public class BossController : MonoBehaviour
 
     public BossAction[] actions;
 
+    private int currentAction;
+    private float actionCounter;
+    private float shotCounter;
+
+    private Vector2 moveDirection;
+    public Rigidbody2D theRB;
+
+    public int currentHealth;
+
+    //public GameObject deadEffect;
+    //public GameObject levelExit;
+
+
     private void Awake()
     {
         instance = this;
@@ -16,13 +29,70 @@ public class BossController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        actionCounter = actions[currentAction].actionLength;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (actionCounter > 0)
+        {
+            actionCounter -= Time.deltaTime;
+
+            moveDirection = Vector2.zero;
+
+            // 移动
+            if (actions[currentAction].shouldMove)
+            {
+                if (actions[currentAction].shouldChasePlayer)
+                {
+                    moveDirection = PlayerController.instance.transform.position - transform.position;
+                    moveDirection.Normalize();
+                }
+
+                if (actions[currentAction].moveToPoint)
+                {
+                    moveDirection = actions[currentAction].pointMoveTo.position - transform.position;
+                    moveDirection.Normalize();
+                }
+            }
+
+            // 射击
+            if (actions[currentAction].shouleShoot)
+            {
+                shotCounter -= Time.deltaTime;
+
+                if (shotCounter <= 0)
+                {
+                    shotCounter = actions[currentAction].timeBetweenShots;
+
+                    foreach(Transform t in actions[currentAction].shotPoints)
+                    {
+                        Instantiate(actions[currentAction].itemToShoot, t.position, t.rotation);
+                    }
+                }
+            }
+
+
+            theRB.velocity = moveDirection * actions[currentAction].moveSpeed;
+
+
+        } else
+        {
+            currentAction = (currentAction + 1) % actions.Length;
+            actionCounter = actions[currentAction].actionLength;
+        }
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            gameObject.SetActive(false);
+        }
     }
 }
 
@@ -35,7 +105,7 @@ public class BossAction
 
     public bool shouldMove;
     public bool shouldChasePlayer;
-    public float speed;
+    public float moveSpeed;
     public bool moveToPoint;
     public Transform pointMoveTo;
 
